23e21c6273a563c5595f17e647a9907fc34cdb49
RMS <30451119+rms-code@users.noreply.github.com>
2018-06-09 12:23:17 -0400

    Add files via upload

---
+++ adminalert.ps1
@@ -0,0 +1,16 @@
+﻿#Groups to watch, add your own.
+$adm_da = (Get-ADGroupMember "Domain Admins").Name
+$adm_ea = (Get-ADGroupMember "Enterprise Admins").Name
+$adm_bia = (Get-ADGroupMember "Administrators").Name
+
+foreach($admin in $adm_da){if($admin -eq "Administrator"){}else{
+Send-MailMessage -From "" -To "" -Subject "USER HAS BEEN ADDED TO DOMAIN ADMINS, IMMEDIATE ATTENTION!" -Body "USER HAS BEEN ADDED TO DOMAIN ADMINS, IMMEDIATE ATTENTION!" -SmtpServer ""
+}}
+
+foreach($admin2 in $adm_ea){if($admin2 -eq "Administrator"){}else{
+Send-MailMessage -From "" -To "" -Subject "USER HAS BEEN ADDED TO ENTERPRISE ADMINS, IMMEDIATE ATTENTION!" -Body "USER HAS BEEN ADDED TO ENTERPRISE ADMINS, IMMEDIATE ATTENTION!" -SmtpServer ""
+}}
+
+foreach($admin3 in $adm_bia){if($admin3 -eq "Administrator" -or $admin3 -eq "Domain Admins" -or $admin3 -eq "Enterprise Admins"){}else{
+Send-MailMessage -From "" -To "" -Subject "USER HAS BEEN ADDED TO BUILT-IN ADMINISTRATORS, IMMEDIATE ATTENTION!" -Body "USER HAS BEEN ADDED TO BUILTIN ADMINISTRATORS, IMMEDIATE ATTENTION!" -SmtpServer ""
+}}
